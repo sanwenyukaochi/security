5ea57a6953ed35be6d59194ce2002b439449277c
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-07-14 18:14:14 +0800

    feat: 202507141814

--- src/main/java/com/sanwenyukaochi/security/security/filter/FirewallRejectionHandlerFilter.java
+++ src/main/java/com/sanwenyukaochi/security/security/filter/FirewallRejectionHandlerFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
@@ -21,7 +22,7 @@ import java.io.IOException;
 public class FirewallRejectionHandlerFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
         try {
             log.info("RequestRejectedExceptionFilter 处理请求: {}", request.getRequestURI());
